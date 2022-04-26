3ac4df19fc340bdfb445fd6f0cde60676949cf1b
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-04-26 13:19:14 +0200

    Add project files.

---
+++ ListaIndirizzi/Indirizzi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Indirizzi
+    {
+
+        public string nome;
+        public string cognome;
+        public string via;
+        public string città;
+        public string provincia;
+        public int codicePostale;
+
+        //creo costruttore
+
+        public Indirizzi(string nome, string cognome, string via, string città, string provincia, int codicePostale)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.via = via;
+            this.città = città;
+            this.provincia = provincia;
+            this.codicePostale = codicePostale;
+
+        }
+
+        //metodo override
+
+        public override string ToString()
+        {
+            string rappresentazioneInStringa = "indirizzo\n";
+            
+            rappresentazioneInStringa += "nome: \t" + this.nome + "\n";
+            rappresentazioneInStringa += "cognome: \t" + this.cognome + "\n";
+            rappresentazioneInStringa += "via: \t" + this.via + "\n";
+            rappresentazioneInStringa += "città: \t" + this.città + "\n";
+            rappresentazioneInStringa += "provincia: \t" + this.provincia + "\n";
+            rappresentazioneInStringa += "codice postale: \t" + this.codicePostale + "\n";
+
+            return rappresentazioneInStringa;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,18 @@
+﻿using ListaIndirizzi;
+
+List<Indirizzi> listaDiIndirizzi = new List<Indirizzi>();
+
+string path = "C:\\Utenti\\Ersi\\Desktop\\esercitazioni-NET\\addresses.csv";
+StreamReader file = File.OpenText(path);
+while (!file.EndOfStream)
+{
+    string riga = file.ReadLine();
+    string[] arrayDiInformazioni = riga.Split(",");
+    string nome = arrayDiInformazioni[0];
+    string cognome = arrayDiInformazioni[1];
+    string via = arrayDiInformazioni[2];
+    string città = arrayDiInformazioni[3];
+    string provincia = arrayDiInformazioni[4];
+    int codicePostale = int.Parse(arrayDiInformazioni[5]);
+
+}
